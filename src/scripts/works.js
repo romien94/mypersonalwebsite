0b2e71f052449eb4e78f6cd3bf576dac90fcf8bd
Roman <romanuskov94@gmail.com>
2020-01-30 07:14:17 +0300

    slider is in progress

--- src/scripts/works.js
+++ src/scripts/works.js
@@ -1,29 +1,62 @@
 import Vue from "vue";
 
 const thumbs = {
-  template: "#slider-thumbs"
-}
+  template: "#slider-thumbs",
+  props:["works","currentWork"]
+};
 
 const buttons = {
   template: "#slider-buttons"
-}
+};
 
 const tags = {
-  template: "#slider-tags"
-}
+  template: "#slider-tags",
+  props:["currentWork"]
+};
 
 const info = {
   template: "#slider-info",
-  components: {tags}
-}
+  components: {tags},
+  props:["currentWork"]
+};
 
 const display = {
   template: "#slider-display",
-  components: {thumbs,buttons}
-}
+  components: {thumbs,buttons},
+  props: ["works"],
+  // methods:{
+  //   handleSlide(direction) {
+  //     this.emit("slide","direction")
+  //   }
+  // }
+};
 
 new Vue({
   el:"#slider-component",
   template: "#slider-container",
-  components: {display, info}
+  components: {display, info},
+  data (){
+    return {
+      works:[],
+      currentWork:{}
+    }
+  },
+  methods: {
+    makeArrWithRequiredImages (data) {
+      return data.map(item=>{
+        const requiredPic = require(`../images/content/slider/${item.photo}`);
+        item.photo = requiredPic;
+        return item;
+      })
+    }
+  },
+  created () {
+    const data = require("../data/works.json");
+    this.works = this.makeArrWithRequiredImages(data);
+    this.currentWork = this.works[0];
+  },
+  handleSlide(direction) {
+    console.log(direction)
+    // this.emit("slide","direction")
+  }
 });
